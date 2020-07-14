a9a2fa661e5fa184cc8d2326d12c4f2bfcfbf18c
debhik <soubhikdas2000@gmail.com>
2020-07-14 16:48:44 +0530

    Updates

---
+++ precache-manifest.309921fd301bd50087a69eb3b412a40d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41a019955273ad0d5efc4fd55f1bafe3",
+    "url": "/react-sortvis/index.html"
+  },
+  {
+    "revision": "4138a287d986f51c3151",
+    "url": "/react-sortvis/static/css/main.0ba61c61.chunk.css"
+  },
+  {
+    "revision": "87f62c831d00e71426bf",
+    "url": "/react-sortvis/static/js/2.fa3e8123.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-sortvis/static/js/2.fa3e8123.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4138a287d986f51c3151",
+    "url": "/react-sortvis/static/js/main.2103588a.chunk.js"
+  },
+  {
+    "revision": "a533a9907889d792e56c",
+    "url": "/react-sortvis/static/js/runtime-main.5201c548.js"
+  }
+]);
